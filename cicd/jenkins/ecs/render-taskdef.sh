4270fb5c263c3b6ed51f3ffba5d3f8f816b0b9aa
lordsesay <lordsesay@outlook.com>
2026-04-26 19:01:54 -0400

    Update

---
+++ cicd/jenkins/ecs/render-taskdef.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -euo pipefail
+
+AWS_REGION="$1"
+
+echo "Registering new ECS task definition"
+
+NEW_TASK_DEF_ARN=$(aws ecs register-task-definition \
+  --cli-input-json file://new-taskdef.json \
+  --region "$AWS_REGION" \
+  --query "taskDefinition.taskDefinitionArn" \
+  --output text)
+
+echo "$NEW_TASK_DEF_ARN" > new-taskdef-arn.txt
+
+echo "Registered task definition: $NEW_TASK_DEF_ARN"
